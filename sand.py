ceacadef389642d1f353b2987d60c901f8f2a916
Kalaborative <kalaborative94@gmail.com>
2022-04-17 14:31:00 -0500

    Heroku push

--- sand.py
+++ sand.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://samp:asdf@localhost/mydb"
 db = SQLAlchemy(app)
+# Adding pointless text
 
 class Admins (db.Model):
     __tablename__ = "admins"
